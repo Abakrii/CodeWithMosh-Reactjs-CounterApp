793343bd19a8e87c4a56e3688fd97f945fffa402
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2019-12-28 17:09:10 +0200

    Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 1
+        count: 0
     }
     render() {
-       return(
-           <React.Fragment>
-               <span>{this.formatCount()}</span>
-                <button>Increment</button>
-           </React.Fragment>
-       );
+        return (
+            <React.Fragment>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </React.Fragment>
+        );
     }
 
 
 
     formatCount() {
-        const {count} = this.state;
+        const { count } = this.state;
         return count === 0 ? 'zero' : count;
     }
 
