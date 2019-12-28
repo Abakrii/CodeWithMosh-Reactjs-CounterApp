a3fc31042ed0133d116885f64f08b3ede5c79646
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2019-12-28 17:34:19 +0200

    Rendring Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: [
+            'tag1',
+            'tag2',
+            'tag3'
+        ]
     }
     render() {
         return (
             <React.Fragment>
                 <span className={this.getBagdeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>{this.getListOfTags()}</ul>
             </React.Fragment>
         );
+    }
+
+
+
+    getListOfTags() {
+     const listOfTags = this.state.tags.map(tag => <li key={tag}>{tag}</li>)
+     return listOfTags
+       
     }
 
 
