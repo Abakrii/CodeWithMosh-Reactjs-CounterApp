362ab04d00565cbaaff0a17dff6ff9c162c453e2
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2019-12-28 18:08:08 +0200

    Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
         tags: [
             'tag1',
             'tag2',
             'tag3'
         ]
     }
+    handleIncrement() {
+        console.log('increment button',this)
+    }
     render() {
         return (
             <React.Fragment>
-                
+
                 <ul>
                     {this.state.tags.length === 0 && 'please Create a new tag'}
                     {this.getListOfTags()}
-                    </ul>
+                </ul>
+                <button onClick ={this.handleIncrement} className={this.getBagdeClasses()}>Increment</button>
             </React.Fragment>
         );
     }
@@ -24,9 +28,9 @@ class Counter extends Component {
 
 
     getListOfTags() {
-     const listOfTags = this.state.tags.map(tag => <li key={tag}>{tag}</li>)
-     return listOfTags
-       
+        const listOfTags = this.state.tags.map(tag => <li key={tag}>{tag}</li>)
+        return listOfTags
+
     }
 
 
