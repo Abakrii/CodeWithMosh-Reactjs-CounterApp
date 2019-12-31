a720fd7a3c3f37265faedcdf742ccdb2dbe529bf
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2020-01-01 01:26:27 +0200

    Destructuring Arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,13 +3,14 @@ import Counter from "./counter";
 class Counters extends Component {
  
   render() {
+      const {onReset, counters, onDelete, onIncrement} = this.props;
     return (
       <div>
-          <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-        {this.props.counters.map(counter => (
+          <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+        {counters.map(counter => (
           <Counter
-           onDelete={this.props.onDelete}  
-           onIncrement={this.props.onIncrement} 
+           onDelete={onDelete}  
+           onIncrement={onIncrement} 
             key={counter.id}
              counter={counter} />
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-const Navbar = (props) => {
-    console.log(props)
+const Navbar = ({totalCounters}) => {
+   
     return (    
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <a className="navbar-brand" href="#">
            Navbar {" "} 
-           <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+           <span className="badge badge-pill badge-secondary">{totalCounters}</span>
           </a>
         </nav>
       );
