6faa2fcc4529fe471a536f02b713e01775c8170b
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2019-12-31 03:29:33 +0200

    Raising and Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,62 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.value,
-        tags: [
-            'tag1',
-            'tag2',
-            'tag3'
-        ]
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"],
+    test: {
+      test1: "ab",
+      test2: "bc",
+      test3: "cd"
     }
+  };
 
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this)
+  // constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this)
+  // }
+  handleIncrement = product => {
+    console.log("product", product);
+
+    this.setState({ value: this.state.value + 1 });
+    // for(let x in this.state.test) {
+    //     console.log(x)
     // }
-    handleIncrement = product => {
-        console.log('product', product)
+  };
 
-        this.setState({ value: this.state.value + 1 })
-    }
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
+  render() {
+    console.log("props", this);
+    return (
+      <div>
+        <span className={this.getBagdeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ product: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button 
+            onClick={this.props.onDelete}
+        className="btn btn-danger btn-sm m-2">Delete</button>
+      </div>
+    );
+  }
 
-    doHandleIncrement = () =>{
-        this.handleIncrement({id:1})
-    }
-    render() {
-        console.log("props", this)
-        return (
-            <div>
-                {this.props.children}
-                <span className={this.getBagdeClasses()}>{this.formatCount()}</span>
+  getListOfTags() {
+    const listOfTags = this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+    return listOfTags;
+  }
 
-                <button 
-                onClick={()=>this.handleIncrement({product:1})} 
-                className="btn btn-secondary btn-sm"
-                >
-                    Increment
-                    </button>
-            </div>
-        );
-    }
-
-
-
-    getListOfTags() {
-        const listOfTags = this.state.tags.map(tag => <li key={tag}>{tag}</li>)
-        return listOfTags
-    }
-
-    getBagdeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'zero' : count;
-    }
-
+  getBagdeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
+  }
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 class Counters extends Component {
-    state = {
-        counters:[
-            {id:1, value:4},
-            {id:2, value:0},
-            {id:3, value:0},
-            {id:4, value:0}
-        ]
-      }
-    render() { 
-        return ( 
-            <div>
-                {this.state.counters.map(counter=>
-                     <Counter key={counter.id} value={counter.value} selected={true}>
-    <h4>test #{counter.id}</h4>
-                     </Counter>
-                     )}
-            </div>
-         );
-    }
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = () => console.log('Handle Event (Delete)');
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter onDelete={this.handleDelete} key={counter.id} value={counter.value} selected={true} />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
